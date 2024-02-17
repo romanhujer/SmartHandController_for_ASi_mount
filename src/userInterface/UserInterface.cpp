fe0fc2c6ce8195701dca38fdd1aa874e0752d156
Roman Hujer <roman@hujer.net>
2024-02-17 12:10:12 +0100

    Debug message

--- src/userInterface/UserInterface.cpp
+++ src/userInterface/UserInterface.cpp
@@ -901,20 +901,20 @@ queryAgain:
    VF("MSG: "); VL(s); 
 #endif   
    if (mDate < rDate) {
-    VLF("MSG: Moute time & date is less than RTC set mount time & date)");
+    VLF("MSG: Mount time & date is less than RTC set mount time & date)");
     sprintf(s, ":SC%02d/%02d/%02d#", rtd.month, rtd.day, rtd.year); onStep.Set(s);
     sprintf(s, ":SL%02d:%02d!%02d#", rtd.hour, rtd.minute, rtd.second); onStep.Set(s);
    } 
    else if ((mDate == rDate) && (mTime < rTime)) {
-     VLF("MSG: Moute time is less than RTC set mount time");
+     VLF("MSG: Mount time is less than RTC set mount time");
      sprintf(s, ":SL%02d:%02d!%02d#", rtd.hour, rtd.minute, rtd.second); onStep.Set(s);
     }
     else {
-      VLF("MSG: Moute time is greater than RTC sync RTC time");
+      VLF("MSG: Mount time is greater than RTC sync RTC time");
       Rtc.set(mtd.year+2000, mtd.month, mtd.day, mtd.hour, mtd.minute, mtd.second);
     }
   }
-  else  { DLF("MSG:  RTC not Ready");}
+  else  { DLF("MSG: RTC not Ready");}
 #endif // end of LOCAL_RTC
 
 again2:
